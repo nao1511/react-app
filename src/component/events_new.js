ca2251c1a9dd7f8ccd28964e346c5b95b0f1a13f
Naofumi Aoyama <nao1511@gmail.com>
2020-09-25 02:47:47 +0900

    create event new component

---
+++ src/component/events_new.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux'
+
+// import { postEvent } from '../actions'
+
+class EventsNew extends Component {
+  render() {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    )
+  }
+}
+
+// const mapDispatchToProps = ({ postEvent })
+
+export default connect(null, null)(EventsNew)
