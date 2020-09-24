9d8eec12bf569501df920437ff97696db8128867
Naofumi Aoyama <nao1511@gmail.com>
2020-09-24 21:03:17 +0900

    change text

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>
+          Hello World
+        </p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
